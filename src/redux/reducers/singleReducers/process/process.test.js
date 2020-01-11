46573b6e513f8d1d0420e0725d6805d8c75bea40
Raisa Primerova <riyuhide@gmail.com>
2020-01-10 21:27:02 -0700

    Test process reducer

--- src/redux/reducers/singleReducers/process/process.test.js
+++ src/redux/reducers/singleReducers/process/process.test.js
@@ -8,15 +8,38 @@ describe("process", () => {
     expect(result).toEqual(expected);
   });
 
-  it("should return process value if type of action is ADD_PROCESS", () => {
+  it("should return process value if type of action is ADD_PROCESS_NAME", () => {
     const mockAction = {
-      type: 'ADD_PROCESS',
-      process: 'login'
+      type: 'ADD_PROCESS_NAME',
+      name: 'login'
     }
 
-    const expected = 'login'
+    const expected = { name: 'login' }
 
     const result = process(undefined, mockAction)
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return process value if type of action is ADD_PROCESS_STEP", () => {
+    const mockAction = {
+      type: 'ADD_PROCESS_STEP',
+      step: 1
+    }
+
+    const expected = { step: 1 }
+
+    const result = process(undefined, mockAction)
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return process value if type of action is INCREASE_STEP", () => {
+    const mockAction = { type: 'INCREASE_STEP' }
+
+    const expected = { step: 2 }
+
+    const result = process({ step: 1 }, mockAction)
 
     expect(result).toEqual(expected);
   });
