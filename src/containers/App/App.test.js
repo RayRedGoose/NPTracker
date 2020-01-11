2870b32f0adb32aa7df1f00672d06fa49500553d
Raisa Primerova <riyuhide@gmail.com>
2020-01-10 21:50:26 -0700

    Test mapStateToProps and mapDispatchToProps in App component

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { App } from './App';
+import { App, mapStateToProps, mapDispatchToProps } from './App';
+import { addUserInfo } from 'redux/actions'
 
 describe('App', () => {
-  it('should match the snapshot with all data passed in correctly', () => {
-    const app = shallow(<App />);
-    expect(app).toMatchSnapshot();
+  let App
+
+  beforeEach(() => {
+    const app = shallow(
+      <App />
+    );
+  });
+
+});
+
+describe("mapStateToProps", () => {
+  it("should return the object with correct key/value pairs from store", () => {
+    const mockStore = {
+      user: { },
+      process: null,
+      parks: []
+    }
+
+    const expected = {
+      user: { },
+      process: null,
+    }
+
+    const result = mapStateToProps(mockStore)
+
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("mapDispatchToProps", () => {
+  let mockDispatch, mockProps
+
+  beforeEach(() => {
+    mockDispatch = jest.fn()
+    mockProps = mapDispatchToProps(mockDispatch);
+  });
+
+  it("should call dispatch with an addUserInfo action when addUserInfo is called", () => {
+    const user = {name: 'Ray'}
+
+    const actionToDispatch = addUserInfo(user)
+
+    const result = mockProps.addUserInfo(user)
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
   });
 });
