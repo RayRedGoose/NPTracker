d2f95ea0214689d24b4dff724724d48588e258f8
Raisa Primerova <riyuhide@gmail.com>
2020-01-12 09:43:16 -0700

    Create park card

--- src/components/ParkCardPreview/ParkCardPreview.js
+++ src/components/ParkCardPreview/ParkCardPreview.js
@@ -1,14 +1,18 @@
 import './ParkCardPreview.scss'
 import React from 'react'
-import starEmpty from 'assets/star-empty.svg'
+import done from 'assets/done.svg'
 import arrow from 'assets/more.svg'
+import bell from 'assets/bell.svg'
 
 const ParkCardPreview = ({id, image, name}) => {
   return (
     <section id={id} className="park-card">
       <div className="preview" style={{backgroundImage: `url(${image})`}}>
         <header>
-          <img src={starEmpty} alt="empty star"/>
+          <section>
+            <img src={done} alt="flag"/>
+            <img src={bell} alt="bell "/>
+          </section>
           <img src={arrow} alt="arrow"/>
         </header>
       </div>
