75dd312c26f278b40b32e4bc2f86f2ca6765db58
Raisa Primerova <riyuhide@gmail.com>
2020-01-12 17:16:06 -0700

    Test ParksContainer and redux map fns

--- src/containers/ParksContainer/ParksContainer.js
+++ src/containers/ParksContainer/ParksContainer.js
@@ -1,15 +1,65 @@
 import './ParksContainer.scss'
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getData } from 'apiCalls'
+import { addParks } from 'redux/actions'
 import ParkCardPreview from 'components/ParkCardPreview/ParkCardPreview'
 
-class ParksContainer extends Component {
+export class ParksContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isLoaded: false,
+      error: null
+    }
+  }
+
+  async componentDidMount() {
+    if (!this.props.parks.length) this.fetchParks()
+  }
+
+  fetchParks = async () => {
+    try {
+      const parks = await getData('parks', 'park')
+      this.setState({isLoaded: true})
+      this.props.addParks(parks)
+    }
+    catch (error) {
+      this.setState({
+        isLoaded: false,
+        error: error.message
+      })
+    }
+  }
+
+  createCards = () => {
+    return this.props.parks.map(park => (
+      <ParkCardPreview
+        key={park.id}
+        id={park.id}
+        image={park.images[0].url}
+        name={park.fullName} />
+    ))
+  }
+
   render() {
     return (
       <section className="parks-content">
-
+        { this.createCards()}
       </section>
     )
   }
 }
 
-export default ParksContainer
+export const mapStateToProps = ({parks}) => ({
+  parks
+})
+
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    addParks
+  }, dispatch)
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(ParksContainer)
---
+++ src/containers/ParksContainer/ParksContainer.test.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ParksContainer, mapStateToProps, mapDispatchToProps } from './ParksContainer';
+import { getData } from 'apiCalls'
+import { addParks } from 'redux/actions'
+
+jest.mock('apiCalls')
+
+describe("ParksContainer", () => {
+  const mockParksData = [{
+    id: 1,
+    fullName: 'Some National Park',
+    description: 'something about park here',
+    images: [{url: 'images/some.jpg'}]
+  }]
+
+  describe("ParksContainer container", () => {
+    let container, instance
+    const addParks = jest.fn()
+
+    beforeEach(() => {
+      container = shallow(
+        <ParksContainer
+          parks={mockParksData}
+          addParks={addParks} />
+      )
+
+      instance = container.instance()
+    });
+
+    it("should match snapshot with all data passed in correctly", () => {
+      expect(container).toMatchSnapshot();
+    });
+
+    it("should call fetchParks method after rendering if there is no parks in props", async () => {
+      const container = shallow(
+        <ParksContainer
+          parks={[]}
+          addParks={addParks} />
+      )
+
+      const instance = container.instance()
+
+      const spy = jest.spyOn(instance, 'fetchParks')
+        .mockImplementation(() => Promise.resolve(mockParksData))
+
+      await instance.componentDidMount()
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it("should call createCards method after rendering", () => {
+      const spy = jest.spyOn(instance, 'createCards')
+        .mockImplementation(jest.fn())
+      instance.forceUpdate()
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    describe("fetchParks", () => {
+      beforeEach(() => {
+        getData.mockImplementation(() => Promise.resolve(mockParksData))
+      });
+
+      it("should call getData when fetchParks is called", async () => {
+
+        await instance.fetchParks()
+
+        expect(getData).toHaveBeenCalledWith('parks', 'park');
+      });
+
+      it("should call addParks prop when fetchParks is called", async () => {
+
+        await instance.fetchParks()
+
+        expect(addParks).toHaveBeenCalledWith(mockParksData);
+      });
+
+      it("should change isLoaded state to true", async () => {
+        expect(container.state('isLoaded')).toEqual(false);
+
+        await instance.fetchParks()
+
+        expect(container.state('isLoaded')).toEqual(true);
+      });
+
+      it("should change isLoaded state to false if fetch is failed", async () => {
+        getData.mockImplementation(() => Promise.reject(Error('Failed to fetch')))
+
+        instance.setState({isLoaded: true})
+
+        expect(container.state('isLoaded')).toEqual(true);
+
+        await instance.fetchParks()
+
+        expect(container.state('isLoaded')).toEqual(false);
+      });
+
+      it("should change error state if fetch is failed", async () => {
+        getData.mockImplementation(() => Promise.reject(Error('Failed to fetch')))
+
+        expect(container.state('error')).toEqual(null);
+
+        await instance.fetchParks()
+
+        expect(container.state('error')).toEqual("Failed to fetch");
+      });
+    });
+  });
+
+  describe("mapStateToProps", () => {
+    it("should return the object with correct keys from store", () => {
+      const mockStore = {
+        parks: mockParksData,
+        user: {},
+        activeTab: 2
+      }
+
+      const expected = {
+        parks: mockParksData
+      }
+
+      const result = mapStateToProps(mockStore)
+
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe("mapDispatchToProps", () => {
+    it("should call dispatch with addParks after addParks prop is called", () => {
+      const mockDispatch = jest.fn()
+      const mockProps = mapDispatchToProps(mockDispatch)
+      const actionToDispatch = addParks(mockParksData)
+
+      mockProps.addParks(mockParksData)
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
+  });
+});
