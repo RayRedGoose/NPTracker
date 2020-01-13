7b1292cff97c2f5a4d8a212dcfeae54fd3a884a5
Raisa Primerova <riyuhide@gmail.com>
2020-01-12 17:15:33 -0700

    Test ParkCardPreview component

---
+++ src/components/ParkCardPreview/ParkCardPreview.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ParkCardPreview from './ParkCardPreview';
+
+it("should match snapshot with all data passed in correctly", () => {
+  const card = shallow(
+    <ParkCardPreview
+      id={1}
+      image="https://www.some.url/preview.jpg"
+      name="Some National Park"/>
+  )
+  expect(card).toMatchSnapshot();
+});
