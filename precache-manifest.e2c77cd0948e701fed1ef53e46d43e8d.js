8c13be2ef26f5f839604993e0a7815c80d3d1c79
Raisa Primerova <riyuhide@gmail.com>
2020-02-10 20:30:06 -0700

    Updates

---
+++ precache-manifest.e2c77cd0948e701fed1ef53e46d43e8d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ebfa073fac7a159ef2578417434100",
+    "url": "/NPtracker/index.html"
+  },
+  {
+    "revision": "2d6beb15ff76669a44ae",
+    "url": "/NPtracker/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "8772860f47097fcc17d0",
+    "url": "/NPtracker/static/css/main.79f9f7c6.chunk.css"
+  },
+  {
+    "revision": "2d6beb15ff76669a44ae",
+    "url": "/NPtracker/static/js/2.19082308.chunk.js"
+  },
+  {
+    "revision": "85701e1121fb954a406d391321a3de9e",
+    "url": "/NPtracker/static/js/2.19082308.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8772860f47097fcc17d0",
+    "url": "/NPtracker/static/js/main.502f5040.chunk.js"
+  },
+  {
+    "revision": "068c060aa2b95aafbc22",
+    "url": "/NPtracker/static/js/runtime-main.7872e03b.js"
+  },
+  {
+    "revision": "b47c8f0b70ab09a6fb3e1afe8c4014fa",
+    "url": "/NPtracker/static/media/balalaika.b47c8f0b.svg"
+  },
+  {
+    "revision": "9a386b3e7d13b966cb33dd185c64c123",
+    "url": "/NPtracker/static/media/bell-full.9a386b3e.svg"
+  },
+  {
+    "revision": "aaeb24f1edd52f7e87081e082fe4e066",
+    "url": "/NPtracker/static/media/bell.aaeb24f1.svg"
+  },
+  {
+    "revision": "a7ba4a966a1e9549fccd9962833647b8",
+    "url": "/NPtracker/static/media/bg-image.a7ba4a96.jpg"
+  },
+  {
+    "revision": "65820460b17637caf9f0f1d059b49c95",
+    "url": "/NPtracker/static/media/checkmark.65820460.svg"
+  },
+  {
+    "revision": "fdb06dd8d0f88b5fb2d84753abad3fd2",
+    "url": "/NPtracker/static/media/logo.fdb06dd8.svg"
+  },
+  {
+    "revision": "3edaf60d3da5e86ce1a9211f4f14dbe8",
+    "url": "/NPtracker/static/media/matryoshka.3edaf60d.svg"
+  },
+  {
+    "revision": "cc3dcfa1fcbd3093f599a6b569aba343",
+    "url": "/NPtracker/static/media/more.cc3dcfa1.svg"
+  },
+  {
+    "revision": "0190f07c18d76ff2de6bc35a039e4fa2",
+    "url": "/NPtracker/static/media/note-1.0190f07c.svg"
+  },
+  {
+    "revision": "45efd08de4c58c4e8cd0c517f2673ff8",
+    "url": "/NPtracker/static/media/note-2.45efd08d.svg"
+  },
+  {
+    "revision": "d7542fbc082b254d215d1233a11e94fe",
+    "url": "/NPtracker/static/media/note-3.d7542fbc.svg"
+  },
+  {
+    "revision": "295dd2adecd607012b1b65d4c1993712",
+    "url": "/NPtracker/static/media/note.295dd2ad.svg"
+  },
+  {
+    "revision": "279f7787be27b1e8f6dbab7f07f6c596",
+    "url": "/NPtracker/static/media/pin.279f7787.svg"
+  },
+  {
+    "revision": "dd0930f535f667c861b186c2f8f67e0e",
+    "url": "/NPtracker/static/media/remove.dd0930f5.svg"
+  },
+  {
+    "revision": "287ce05033f02603321d1aa75ff961a2",
+    "url": "/NPtracker/static/media/search.287ce050.svg"
+  },
+  {
+    "revision": "254114a84a46aeb3b3727dd7baaa4dda",
+    "url": "/NPtracker/static/media/send.254114a8.svg"
+  },
+  {
+    "revision": "6aba03ab3a1711c77c24d59f9e5b7c49",
+    "url": "/NPtracker/static/media/traveler.6aba03ab.svg"
+  }
+]);
